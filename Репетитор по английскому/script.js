2c13f8695b2ed42f1f754676fd6b9bc48cd54fe4
Huzhva-Yaroslav <91682192+Huzhva-Yaroslav@users.noreply.github.com>
2022-07-20 21:10:00 +0300

    Add files via upload

--- Репетитор по английскому/script.js
+++ Репетитор по английскому/script.js
@@ -1,8 +1,66 @@
-$(".multiple-items").slick({
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 3
-  });
-  
+$('.multiple-items').slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: false,
+        dots: false,
+        arrows: false,
+      }
+    },
+    {
+      breakpoint: 760,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 2,
+        arrows: false,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+      }
+    }
+   
+  ]
+});
+ 
+// Counter
+const counters = document.querySelectorAll('.count');
+const speed = 200;
+const count = [];
+counters.forEach((counter) => {
+  count.push(counter);
+  const updateCount = () => {
+    const target = parseInt(counter.getAttribute("data-target"));
+    const count = parseInt(counter.innerText);
+    const increment = Math.trunc(target / speed);
+    if (count < target) {
+      counter.innerText = count + increment;
+      setTimeout(updateCount, 1);
+    } else {
+      count.innerText = target;
+    }
+  };
+  updateCount();
+});
+window.addEventListener('scroll', ()=>{
+  let y = window.scrollY;
+  if (y > 3700) {
+    count.forEach(element => {
+      element.classList.add('active');
+      console.log(element);
+    });
+  }
+
+});
